d2c59fa431ef60a25aefc549271d9eb108b208c3
= <=>
2023-01-29 15:08:52 +0100

    Ajout de la tranche de fromage

--- Source Code/src/main/java/yt/dasnilo/raclettemod/contents/RacletteItems.java
+++ Source Code/src/main/java/yt/dasnilo/raclettemod/contents/RacletteItems.java
@@ -16,7 +16,9 @@ public class RacletteItems {
     public static final RegistryObject<Item> COUPELLE_SALE = ITEMS.register("coupelle_sale", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC).tab(RacletteTab.RACLETTE_TAB)));
     // Nourritures
     public static final RegistryObject<Item> FROMAGE = ITEMS.register("fromage", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).tab(RacletteTab.RACLETTE_TAB)));
+    public static final RegistryObject<Item> TRANCHE_FROMAGE = ITEMS.register("tranche_fromage", () -> new Item(new Item.Properties().tab(RacletteTab.RACLETTE_TAB).tab(CreativeModeTab.TAB_FOOD)));
     public static final RegistryObject<Item> COUPELLE_FROMAGE = ITEMS.register("coupelle_fromage", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD).tab(RacletteTab.RACLETTE_TAB)));
+    
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
