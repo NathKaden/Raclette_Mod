331b6daeda6a6f0ef8b21df127d04b35f09761db
= <=>
2023-03-23 10:08:25 +0100

    Jai essayé mais ca a pas marché

--- Source Code/src/main/java/yt/dasnilo/raclettemod/contents/RacletteItems.java
+++ Source Code/src/main/java/yt/dasnilo/raclettemod/contents/RacletteItems.java
@@ -1,7 +1,6 @@
 package yt.dasnilo.raclettemod.contents;
 
 
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
