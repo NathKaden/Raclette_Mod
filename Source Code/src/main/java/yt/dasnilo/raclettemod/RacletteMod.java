cb04f755e6cbf23e4c28bb8ce9cff292cf0ec56d
= <=>
2023-03-23 10:46:51 +0100

    HAP !

--- Source Code/src/main/java/yt/dasnilo/raclettemod/RacletteMod.java
+++ Source Code/src/main/java/yt/dasnilo/raclettemod/RacletteMod.java
@@ -7,7 +7,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
-import yt.dasnilo.raclettemod.contents.RacletteBlocks;
 import yt.dasnilo.raclettemod.contents.RacletteItems;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -20,10 +19,7 @@ public class RacletteMod
   public RacletteMod() {
       IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
       logger.info("Mod démarré avec succés !");
-
       RacletteItems.register(modEventBus);
-      RacletteBlocks.register(modEventBus);
-
       MinecraftForge.EVENT_BUS.register(this);
   }
 
